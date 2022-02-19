3d7981c33e1a5a3358f3b523cdf5119da6a497ba
flying-turtle001 <justin.esposito@gmx.de>
2022-02-19 10:23:21 +0100

    adjust styling of todolist

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -15,31 +15,35 @@ const Todo = (props) => {
   };
 
   return (
-    <BaseRow>
-      <div className={styles.wrapper}>
-        <div
-          onClick={changeTodoCompletedStateHandler}
-          className={`${styles.circle} ${
-            props.todo.completed && styles.completed
-          }`}
-        >
-          {props.todo.completed && <img src={iconCheck} alt="icon-check" />}
+    <div className={styles.container}>
+      <BaseRow>
+        <div className={styles.wrapper}>
+          <div className={styles["wrapper-circle"]}>
+            <div
+              onClick={changeTodoCompletedStateHandler}
+              className={`${styles.circle} ${
+                props.todo.completed && styles.completed
+              }`}
+            >
+              {props.todo.completed && <img src={iconCheck} alt="icon-check" />}
+            </div>
+          </div>
+          <span
+            className={`${styles.text} ${
+              props.todo.completed && styles["todo-completed"]
+            }`}
+          >
+            {props.todo.text}
+          </span>
+          <img
+            src={iconCross}
+            alt="icon-cross"
+            className={styles["icon-cross"]}
+            onClick={removeTodoHandler}
+          />
         </div>
-        <span
-          className={`${styles.text} ${
-            props.todo.completed && styles["todo-completed"]
-          }`}
-        >
-          {props.todo.text}
-        </span>
-        <img
-          src={iconCross}
-          alt="icon-cross"
-          className={styles["icon-cross"]}
-          onClick={removeTodoHandler}
-        />
-      </div>
-    </BaseRow>
+      </BaseRow>
+    </div>
   );
 };
 
