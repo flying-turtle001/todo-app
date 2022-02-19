5dc4fe81f29ee926353790471603bc70d39a937a
flying-turtle001 <justin.esposito@gmx.de>
2022-02-19 11:09:54 +0100

    small adjustment

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -18,15 +18,13 @@ const Todo = (props) => {
     <div className={styles.container}>
       <BaseRow>
         <div className={styles.wrapper}>
-          <div className={styles["wrapper-circle"]}>
-            <div
-              onClick={changeTodoCompletedStateHandler}
-              className={`${styles.circle} ${
-                props.todo.completed && styles.completed
-              }`}
-            >
-              {props.todo.completed && <img src={iconCheck} alt="icon-check" />}
-            </div>
+          <div
+            onClick={changeTodoCompletedStateHandler}
+            className={`${styles.circle} ${
+              props.todo.completed && styles.completed
+            }`}
+          >
+            {props.todo.completed && <img src={iconCheck} alt="icon-check" />}
           </div>
           <span
             className={`${styles.text} ${
